c30e81a6325fb17e6922761ecf5aea7bd7f09be6
EmanuelPrgic <emanuel.prgic@fpmoz.sum.ba>
2023-02-14 16:25:44 +0100

    navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,45 +1,106 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        @media screen and (min-width:1024px){ .header_area_container{
+            background:purple;
+        }
+            .nav-image {
+                max-width: 10px;
+                max-height: 10px;
+            }
+        }
 
+            .wgl-header-row_wrapper{
+                background:white;
+            }
+
+            .menu-item-1782{
+                background:#35a07b;
+                margin-left: 250px;
+                border-left: 1px solid white;
+                height: 500px;
+            }
+
+            .menu-item-1810{
+                background:#35a07b;
+                border-left: 1px solid white;
+                height: 500px;
+            }
+
+            .menu-item-1784{
+                background:#35a07b;
+                border-left: 1px solid white;
+                height: 500px;
+            }
+
+            .menu-item-1805{
+                background:#35a07b;
+                border-left: 1px solid white;
+                height: 500px;
+            }
+
+            .menu-item-1832{
+                background:#35a07b;
+                border-left: 1px solid white;
+                height: 500px;
+            }
+
+            .menu-item-1826{
+                background:#35a07b;
+                border-left: 1px solid white;
+                height: 500px;
+            }
+
+            .side_panel .side_panel_inner .side_panel-toggle{
+                color:black;
+                margin-top:400px;
+
+            }
+
+            #menu-main-1{
+                margin-left:140px
+            }
+    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#"><div class="shrink-0 flex items-center">
-            <a href="/profiles/{{Auth::user()->id}}">
-                <img src="/storage/{{Auth::user()->image}}" class="rounded-circle nav-image" alt="Upload a picture">
-            </a>
-        </div></a>
-    <div class="collapse navbar-collapse" id="navbarText">
+    <a class="navbar-brand" href="/profiles/{{Auth::user()->id}}"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="/dashboard">Dashboard</a>
+                <a class="nav-link" href="/dashboard">Dashboard <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/profiles">Profiles</a>
+            <li class="nav-item active">
+                <a class="nav-link" href="/profiles">Profiles <span class="sr-only">(current)</span></a>
             </li>
         </ul>
-        <div class="space-y-1">
-            <x-responsive-nav-link :href="route('profile.edit')">
+        <div>
+            <x-dropdown-link :href="route('profile.edit')">
                 {{ __('Account Settings') }}
-            </x-responsive-nav-link>
-
-            <!-- Authentication -->
-            <form method="POST" action="{{ route('logout') }}">
+            </x-dropdown-link>
+            <form method="POST" action="{{ route('logout') }} ">
                 @csrf
-
-                <x-responsive-nav-link :href="route('logout')"
-                                       onclick="event.preventDefault();
-                                        this.closest('form').submit();">
+                <x-dropdown-link :href="route('logout')"
+                                 onclick="event.preventDefault();
+                                                this.closest('form').submit();">
                     {{ __('Log Out') }}
-                </x-responsive-nav-link>
+                </x-dropdown-link>
             </form>
         </div>
     </div>
 </nav>
-
-<style>
-    .nav-image
-    {
-        width: 50px;
-        height: 50px;
-    }
-</style>
-
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
